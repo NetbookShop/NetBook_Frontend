f3a44130e7b7a7cefa2c55e9558a8361bc594b0d
pikoUsername <galymzhan.amantaj@gmail.com>
2024-02-24 10:40:24 +0600

    Sync update

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,9 +1,17 @@
+import { NavLink } from "react-router-dom";
+import "./Navbar.css"
+import React from "react";
+import logo from "../../Static/karma-systemlogo.png"
+
+
 const NavbarComponent: React.FC = () => { 
     
     return ( 
-        <div className="navbar">
-            
-        </div>
+        <header className="navbar-root">
+            <NavLink to="/" className="home-link">
+                <img src={logo} alt="" />
+            </NavLink>
+        </header>
     )
 }
 
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react',
+                '@babel/preset-typescript',
+              ],
+            },
+          },
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
