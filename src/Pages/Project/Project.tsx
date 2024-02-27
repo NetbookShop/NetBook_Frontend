f5885a9584dbe92e9b20d37c3902435309fc641e
pikoUsername <galymzhan.amantaj@gmail.com>
2024-02-27 16:44:29 +0600

    Making project page

--- src/Pages/Project/Project.tsx
+++ src/Pages/Project/Project.tsx
@@ -1,11 +1,17 @@
+import NavigationMapComponent from "../../Components/NavigationMap/NavigationMap";
 import { NavProps } from "../../Utils/Types";
+import data from "../../TestData/Project.json"
 
 const ProjectPage: React.FC<NavProps> = (props: NavProps) => { 
+    let project = data.project
     props.setCategory("projects")
+    let elemMaps = new Map<string, string>()
+    elemMaps.set("Проекты", "/projects")
+    elemMaps.set(project.name, "/")
 
     return (
         <div className="projectpage-root">
-
+            <NavigationMapComponent elements={elemMaps}/>
         </div>
     )
 }
