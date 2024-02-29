cde8fbb927eee24ff0086b263d786909536f746f
pikoUsername <galymzhan.amantaj@gmail.com>
2024-02-29 18:20:11 +0600

    Sync commit

--- src/Pages/User/User.tsx
+++ src/Pages/User/User.tsx
@@ -1,5 +1,6 @@
 import { NavProps } from "../../Utils/Types";
 import data from "../../TestData/User.json"
+import "./User.css"
 
 const UserPage: React.FC<NavProps> = (props: NavProps) => { 
     props.setCategory("teams")
@@ -8,6 +9,18 @@ const UserPage: React.FC<NavProps> = (props: NavProps) => {
         <div className="user-root">
             <div className="user-banner">
                 <img src={data.banner.fileUrl} alt="banner" width={"100%"} height={"200px"}/>
+            </div>
+            <div className="user-container">
+                <div className="user-left-navbar-main">
+                    <div className="user-avatar">
+                        <img src={data.avatar.fileUrl} alt="" className="user-avatar-icon" width={"160px"} height={"160px"}/>
+                    </div>
+                    <div className="user-left-navbar-info">
+                        <h1 className="">
+
+                        </h1>
+                    </div>
+                </div>
             </div>
         </div>
     )
