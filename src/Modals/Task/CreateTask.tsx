405fb1b0cb3d4e29ceaab17f57bd81980934b135
pikoUsername <galymzhan.amantaj@gmail.com>
2024-03-04 18:16:55 +0500

    Sync commit

--- src/Modals/Task/CreateTask.tsx
+++ src/Modals/Task/CreateTask.tsx
@@ -1,5 +1,11 @@
-const CreateTaskModalCall = () => {
+import { ModalProps } from "../../Utils/Types";
 
+const CreateTaskModal: React.FC<ModalProps> = (props: ModalProps) => {
+    return ( 
+        <div className="create-task-root">
+            
+        </div>
+    )
 }
 
-export default CreateTaskModalCall; 
+export default CreateTaskModal; 
--- src/Utils/Types.tsx
+++ src/Utils/Types.tsx
@@ -3,3 +3,5 @@ import {Dispatch } from 'react'
 export type NavigationCategoryTypes = "projects" | "work" | "teams" | "nochoice" | "none"
 
 export type NavProps = { setCategory: Dispatch<NavigationCategoryTypes> }
+
+export type ModalProps = { setIsOpenModal: Dispatch<boolean>} 
