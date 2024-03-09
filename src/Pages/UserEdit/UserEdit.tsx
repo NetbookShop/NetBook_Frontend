5ba8faa5294ff2ec71427479e08a869c70fe4125
pikoUsername <galymzhan.amantaj@gmail.com>
2024-03-09 16:28:24 +0600

    Now edit user page works

--- src/Pages/UserEdit/UserEdit.tsx
+++ src/Pages/UserEdit/UserEdit.tsx
@@ -1,31 +1,91 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NavProps } from "../../Utils/Types";
 import NavigationMapComponent from "../../Components/NavigationMap/NavigationMap";
 import "./UserEdit.css"
 import ActionsButtonsComponent from "../../Components/Actions/Actions";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { FileApi, FileModel, UserControllersApi } from "task-manager";
+import { ApiConfig, asFileUrl } from "../../Gateway/Config";
+import ErrorMessage from "../../Components/ErrorMessage/ErrorMessage";
+import { upload } from "@testing-library/user-event/dist/upload";
 
 const UserEditPage: React.FC<NavProps> = (props: NavProps) => { 
     const navigate = useNavigate()
+    const { id } = useParams()
 
     props.setCategory("nochoice")
 
-    const [email, setEmail] = useState(props.user?.email)
-    const [name, setName] = useState(props.user?.fullName)
-    const [password, setPassword] = useState<string>()
-    const [repeatPassword, setReapeatPassword] = useState<string>() 
+    const [email, setEmail] = useState<string>('')
+    const [name, setName] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const [repeatPassword, setReapeatPassword] = useState<string>('') 
+    const [ uploadedIcon, setUploadedIcon ] = useState<FileModel>()
+    const [ image, setImage ] = useState<string | ArrayBuffer | null>()
+    const [errorMessage, setErrorMessage] = useState<string>()
+
+    const userApi = new UserControllersApi(ApiConfig)
 
     const editUserSubmit = async () => { 
+        if (password !== undefined || password !== "") { 
+            if (password !== repeatPassword) { 
+                setErrorMessage("Пароли не совпадают")
+                return
+            }
+        }
+        try { 
+            if (props.user !== undefined) { 
+                await userApi.updateUser(id || "", { 
+                    email: props.user.email !== email ? email : null,
+                    fullName: props.user.fullName !== name ? name : null, 
+                    password: password !== undefined || password !== null || password != "" ? password : null,  
+                    avatarId: uploadedIcon?.id, 
+                })
+                navigate("/"); 
+            } 
+        } catch (e) { 
+            console.error(e)
+        }
+    }
 
+    useEffect(() => { 
+        (async () => { 
+            try { 
+                console.log(email, name)
+                if (email !== "" || name !== "") { 
+                    return 
+                }
+                let response = await userApi.getMe()        
+                setEmail(response.data.email)   
+                setName(response.data.fullName)
+            } catch (e) { 
+                console.error(e)
+            }
+        })()
+    }, [])
+
+    const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => { 
+        if (event.target.files === null) { 
+            return  
+        }
+
+        const fileApi = new FileApi(ApiConfig)
+        const file = event.target.files[0]
+    
+
+        const reader = new FileReader();
+        reader.readAsDataURL(file)
+
+        reader.onloadend = () => {
+            setImage(reader.result);
+        }
+        let fileResponse = await fileApi.uploadFileForm(structuredClone(file))
+        setUploadedIcon(fileResponse.data)
     }
-    if (props.user === undefined) { 
-        return <div></div> 
-    }
-    const user = props.user
+
     
     let elements = new Map<string, string>()
     elements.set("Проекты", "/projects")
-    elements.set(user.fullName || "", `/user/${user.id}/edit`)
+    elements.set(props.user?.fullName || "", `/user/${props.user?.id}/edit`)
 
 
     return (
@@ -34,54 +94,78 @@ const UserEditPage: React.FC<NavProps> = (props: NavProps) => {
             <div className="useredit-container">
                 <h1 className="useredit-header">Управление аккаунтом</h1>
 
+                <img src={
+                        (
+                            image === null ||
+                            image === undefined
+                        )
+                        ? asFileUrl(props.user?.avatar?.filePath)
+                        : String(image)}
+                        alt=""
+                        className="projectedit-project-icon"
+                    />
+                    <label htmlFor="photoInput" className='projectedit-project-icon-change'>
+                        <input
+                            type="file"
+                            id="photoInput"
+                            accept="image/*"
+                            onChange={handleFileChange}
+                            style={{ display: 'none' }}
+                        />
+                        <div className="projectedit-icon-button">Изменить аватар</div>
+                    </label>
                 <div className="useredit-fields">
                     <div className="useredit-input-field">
                         <label htmlFor="name">Имя пользвателя</label>
                         <input 
                             type="text" 
+                            autoComplete="new-password"
                             className="useredit-input" 
                             id="name" 
                             placeholder="Ваше имя" 
                             value={name}
-                            onChange={(e) => setName(e.target.value)}
+                            onChange={(e) => { e.preventDefault(); setName(e.target.value)} }
                         />
                     </div>
                     <div className="useredit-input-field">
                         <label htmlFor="email">Email пользвателя</label>
                         <input 
                             type="email" 
+                            autoComplete="new-password"
                             className="useredit-input" 
                             id="email" 
                             placeholder="Ваше эмейл" 
                             value={email}
-                            onChange={(e) => setEmail(e.target.value)}
+                            onChange={(e) => { e.preventDefault(); setEmail(e.target.value)} }
                         />
                     </div>
                     <div className="useredit-input-field">
                         <label htmlFor="password">Введите новый пароль</label>
                         <input 
+                            autoComplete="new-password"
                             type="password" 
                             className="useredit-input" 
                             id="password" 
                             placeholder="Ваш новый пароль" 
-                            value={email}
-                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                            onChange={(e) => { e.preventDefault(); setPassword(e.target.value)} }
                         />
                     </div>
                     <div className="useredit-input-field">
                         <label htmlFor="password-repeat">Повторите новый пароль</label>
                         <input 
+                            autoComplete="new-password"
                             type="password" 
                             className="useredit-input" 
                             id="password-repeat" 
                             placeholder="Повторите пароль" 
-                            value={email}
-                            onChange={(e) => setReapeatPassword(e.target.value)}
+                            value={repeatPassword}
+                            onChange={(e) => { e.preventDefault(); setReapeatPassword(e.target.value)} }
                         />
                     </div>
                 </div>
-
-                <ActionsButtonsComponent submitAction={editUserSubmit} cancelAction={() => navigate("/")} /> 
+                <ErrorMessage errorMessage={errorMessage} setErrorMessage={setErrorMessage}/> 
+                <ActionsButtonsComponent submitAction={editUserSubmit} cancelAction={() => navigate("/")} submitText="Сохранить"/> 
             </div>
         </div>
     )
